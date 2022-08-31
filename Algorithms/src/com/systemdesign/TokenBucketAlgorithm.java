d38e5b0e7db10446097b5efc47fc8c53f32f0d32
aditya <adityapandey1207@gmail.com>
2022-08-31 23:25:02 +0530

    System design : Token bucket algo for rate limiter

---
+++ Algorithms/src/com/systemdesign/TokenBucketAlgorithm.java
@@ -0,0 +1,30 @@
+package com.systemdesign;
+
+public class TokenBucketAlgorithm {
+   private long maxBucketSize;
+   private long refillRate ; // per second
+
+  private long currentBucketSize;
+  private long lastRefillTimeStamp; //time in nano seconds
+
+  public synchronized boolean allowRequest(int requestedTokens){
+    refill();
+    
+    if(requestedTokens > currentBucketSize)
+      return false;
+    else
+      currentBucketSize -= requestedTokens ;
+
+    return true;
+  }
+
+  private void refill(){
+    long now = System.nanoTime();
+    long timeElapsed = now - lastRefillTimeStamp;
+    double tokensToAdd = (timeElapsed/Math.pow(10,9))*(refillRate) ;
+    currentBucketSize = Math.min(currentBucketSize+(long) tokensToAdd , maxBucketSize);
+    lastRefillTimeStamp =  now;
+  }
+
+
+}
